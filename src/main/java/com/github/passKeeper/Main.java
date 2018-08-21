5604fe8fec5938b2b097617f62afa8ab00f594c5
Sebastian Wojnicz <sebulw@gmail.com>
2018-08-21 09:12:18 +0200

    Feature/password generator test (#3)
    * Generator bug fix
    * Added generating with digits and special signs

--- src/main/java/com/github/passKeeper/Main.java
+++ src/main/java/com/github/passKeeper/Main.java
@@ -3,11 +3,13 @@ package com.github.passKeeper;
 public class Main {
     public static void main(String[] args) {
 
-        PasswordGenerator passwordGenerator = new PasswordGenerator.PasswordGeneratorBuilder()
+        PasswordGenerator passwordGenerator = new PasswordGeneratorBuilder()
                 .useLower(true)
                 .useUpper(true)
+                .useNumbers(true)
+                .useSpecialSigns(true)
                 .build();
-        String password = passwordGenerator.generate(12);
+        String password = passwordGenerator.generate(20);
 
         System.out.println(password);
     }
